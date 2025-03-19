3463437d1c35ed813d390508e5c1f95bac7db73a
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-19 16:41:42 -0400

    did some more work

--- demo/src/test/java/com/tutorial/demo/student/StudentRepositoryTest.java
+++ demo/src/test/java/com/tutorial/demo/student/StudentRepositoryTest.java
@@ -12,9 +12,20 @@ import java.time.Month;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+//we need to add the @DataJpaTest annotation to make sure that everything works properly 
 @DataJpaTest
 //to make sure that our testing is not conflicting with our actual Postgresql database, we will be using an in memory database
 //add the h2 dependency database
+
+//we also need to create a resources folder in the test folder, and then make another application.properties
+//intially, copy paste the configurations from the original application.properties
+//replace the spring.datasource.url with this line below
+//spring.datasource.url=jdbc:h2://mem:db;DB_CLOSE_DELAY=-1
+//set both the username and the password to sa
+//change this line: spring.jpa.hibernate.ddl-auto to create drop
+//spring.jpa.hibernate.ddl-auto=create-drop
+//add this line to the application.properties
+//spring.datasource.driver-class-name=org.h2.Driver
 
 class StudentRepositoryTest {
     @Autowired
