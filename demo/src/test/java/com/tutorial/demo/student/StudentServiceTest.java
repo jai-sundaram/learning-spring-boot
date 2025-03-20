c8d232dee77c0de135f837d9db98acba432ba60c
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-19 20:40:39 -0400

    finished learning

--- demo/src/test/java/com/tutorial/demo/student/StudentServiceTest.java
+++ demo/src/test/java/com/tutorial/demo/student/StudentServiceTest.java
@@ -4,12 +4,27 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.lang.IllegalStateException;
+
+
+
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 //make sure to add this annotation
 //it enables mockito support, and integrates mockito into the test
@@ -51,8 +66,53 @@ class StudentServiceTest {
 
     @Test
     void addNewStudent() {
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                LocalDate.of(2000, Month.JANUARY,5)
+        );
+        underTest.addNewStudent(student);
+        //we need to use an ArgumentCaptor
+        //we want to verify that the repository was called with save
+        //the whole StudentArgumentCaptor piece helps us capture the actual student that was passed inside this save method
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        //getting the value, which is a student, that was stored
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        //do an assertion statement
+        //checking if that student that was captured is the same as the student that was passed in
+        assertThat(capturedStudent).isEqualTo(student);
+
     }
 
+    @Test
+    void throwWhenEmailIsTaken(){
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                LocalDate.of(2000, Month.JANUARY,5)
+        );
+        //in this case, we know that a student with the same email is already in the db
+        //so the repository method, findStudentByEmail, would return a student
+        //it should return the student object we previously added
+
+        ///heres how we do that
+        given(studentRepository.findStudentByEmail(student.getEmail())).willReturn(Optional.of(student));
+
+        //if there is already a student with the email, the new student should not be added, and instead an exception needs to be thrown
+        //we know that an exception will be thrown because there is already a student with the same email in the system
+        //there will be a certain message associated with that exception
+        //will also have to specify what kind of exception was thrown
+        assertThatThrownBy(()->underTest.addNewStudent(student)).isInstanceOf(IllegalStateException.class).hasMessageContaining("email taken");
+
+        //since we are throwing the exception, the save repo method will never actually be called
+        //we need to make sure that this is the case
+        //we are basically saying that the mock here never saves any student
+        verify(studentRepository, never()).save(any());
+
+
+    }
     @Test
     void updateStudent() {
     }
